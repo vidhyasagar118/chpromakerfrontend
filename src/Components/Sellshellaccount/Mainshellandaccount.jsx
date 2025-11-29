408fe28ad2f9b17e04cde28a9e1b60de33d56d2a
Abhishek <abhishek@example.com>
2025-11-29 19:19:44 +0530

    Initial commit

--- src/Components/Sellshellaccount/Mainshellandaccount.jsx
+++ src/Components/Sellshellaccount/Mainshellandaccount.jsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import "./Mainshellandaccount.css";
-
-// Correct Imports
-import Sellshop from './Sellshop';
+import Sellshop from './sellshop';
 import Mydata from './Mydata';
 import Groupshellinfo from './Groupshellinfo';
-
 const Mainshellandaccount = () => {
   const navigate = useNavigate();
-
   return (
     <div className='Mainshellandaccount'>
-      <button onClick={() => navigate('/sellshop')}>Shellinfo</button>
-      <button onClick={() => navigate('/mydata', { state: { shopname: '' } })}>
-        My Account data
-      </button>
-      <button onClick={() => navigate('/groupshellinfo')}>
-        Group member info
-      </button>
+ <button onClick={() => navigate('/Sellshop')}>Shellinfo </button>
+<button onClick={() => navigate('/Mydata'  ,{ state: { shopname: '' } })}>My Account data</button>
+<button onClick={() => navigate('/Groupshellinfo')}>Group member info</button>
     </div>
-  );
-};
+  )
+}
 
-export default Mainshellandaccount;
+export default Mainshellandaccount
