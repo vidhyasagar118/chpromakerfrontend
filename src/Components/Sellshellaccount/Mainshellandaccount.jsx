f704d963e81845b87b505fe9489bd79f2940b6d0
Abhishek <abhishek@example.com>
2026-01-05 11:32:14 +0530

    Updated backend URLs to deployed API

--- src/Components/Sellshellaccount/Mainshellandaccount.jsx
+++ src/Components/Sellshellaccount/Mainshellandaccount.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import "./Mainshellandaccount.css";
-import Sellshop from './sellshop';
+import Sellshop from './Sellshop.jsx';
 import Mydata from './Mydata';
 import Groupshellinfo from './Groupshellinfo';
 const Mainshellandaccount = () => {
